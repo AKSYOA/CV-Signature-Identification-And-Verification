d6658b635d6c6aff1aab65e419cf3e3cd2d9a0d8
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2022-12-06 14:40:06 +0200

    Reformatted the types of dataset to be trained

--- main.py
+++ main.py
@@ -8,12 +8,19 @@ import numpy as np
 data_path = 'data/'
 
 
-def get_Dataset():
+def get_dataset():
     train_data_path, test_data_path = get_images_paths()
     train_data = read_images(train_data_path)
     test_data = read_images(test_data_path)
-    print(len(train_data))
-    print(len(test_data))
+    X_train, Y_train = reformat_dataset(train_data)
+    X_test, Y_test = reformat_dataset(test_data)
+    return X_train, Y_train, X_test, Y_test
+
+
+def reformat_dataset(data):
+    X = np.array([i[0] for i in data], dtype=object)
+    Y = np.array([i[1] for i in data])
+    return X, Y
 
 
 def get_images_paths():
@@ -40,6 +47,7 @@ def read_images(images_paths):
     images = []
     for i in images_paths:
         image = cv2.imread(i, 0)
+        image = cv2.resize(image, (227, 227))
         image_label = create_label(i)
         images.append([np.array(image), image_label])
     return images
@@ -53,3 +61,6 @@ def create_label(image_path):
         if image_label.endswith(image_classes[i]):
             label_encoded[i] = 1
     return label_encoded
+
+
+get_dataset()
