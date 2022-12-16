48a84152c6ac91705fe0360b45a4e259dc41396d
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2022-12-16 21:59:03 +0200

    Implemented HOG model with 0.975 Accuracy

--- Stage 1/HOG_Model.py
+++ Stage 1/HOG_Model.py
@@ -1,6 +1,18 @@
 import Data_Preparation
+from sklearn.metrics import confusion_matrix, accuracy_score, ConfusionMatrixDisplay
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
 
 X_train, Y_train, X_test, Y_test = Data_Preparation.get_dataset(model_type='HOG', image_size=64)
 
-print(X_train.shape)
-print(Y_train.shape)
+clf = LogisticRegression().fit(X_train, Y_train)
+
+predictions = clf.predict(X_test)
+
+print("Testing Accuracy: " + str(accuracy_score(Y_test, predictions)))
+
+matrix = confusion_matrix(Y_test, predictions)
+
+disp = ConfusionMatrixDisplay(confusion_matrix=matrix)
+disp.plot()
+plt.show()
