21f8d42885598186d48c4e9718519abe947743a0
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2022-12-20 03:47:26 +0200

    Added Siamese Class

--- Stage 2/Siamese.py
+++ Stage 2/Siamese.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import accuracy_score
 import Data_Preparation
 from Siamese_utilities import *
+from tensorflow.keras.optimizers import Adam
+
 import matplotlib.pyplot as plt
 import cv2
 
@@ -10,3 +12,7 @@ test_triplets = get_triplet(test_images)
 
 siamese_network = get_siamese_network()
 siamese_network.summary()
+
+siamese_model = SiameseModel(siamese_network)
+optimizer = Adam(learning_rate=1e-3, epsilon=1e-01)
+siamese_model.compile(optimizer=optimizer)
