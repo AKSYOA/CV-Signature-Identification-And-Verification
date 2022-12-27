c861d55a331ea63d78140f2ddb83c3bea6a9d6c2
Ahmed Abd El-Gawad <ahmedabdelgowadramadan@gmail.com>
2022-12-27 08:09:40 +0200

    Add Training and testing times for stage1, Update documentation

--- Stage1/CNN_Model.py
+++ Stage1/CNN_Model.py
@@ -1,14 +1,20 @@
 import CNN_utilities
 import Data_Preparation
+import time
 
 
 train_data, test_data = Data_Preparation.get_dataset(model_type='CNN', image_size=128)
 
+training_start_time = time.time()
 model = CNN_utilities.generate_CNN_model(train_data)
+training_stop_time = time.time()
 
+testing_start_time = time.time()
 CNN_utilities.test_model(test_data, model, visualise=True)
+testing_stop_time = time.time()
 
 
-
+print(f"Training time: {training_stop_time - training_start_time}s")
+print(f"Testing time: {testing_stop_time - testing_start_time}s")
 
 
--- Stage1/HOG_Model.py
+++ Stage1/HOG_Model.py
@@ -1,8 +1,16 @@
 import Data_Preparation
 import HOG_utilities
+import time
 
 train_data, test_data = Data_Preparation.get_dataset(model_type='HOG', image_size=64)
+
+training_start_time = time.time()
 clf = HOG_utilities.generate_HOG_model(train_data)
+training_stop_time = time.time()
 
+testing_start_time = time.time()
 HOG_utilities.testModel(test_data, clf, visualise=True)
+testing_stop_time = time.time()
 
+print(f"Training time: {training_stop_time - training_start_time}s")
+print(f"Testing time: {testing_stop_time - testing_start_time}s")
